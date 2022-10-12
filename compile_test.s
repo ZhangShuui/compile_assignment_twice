e845aeb0dd2224b9155d9973f9e9d9062fa93901
ZhangShuui <1262426565@qq.com>
2022-10-12 05:52:29 -0700

    make some explanation of the assembly code

--- compile_test.s
+++ compile_test.s
@@ -1,16 +1,15 @@
-	.arch armv7-a
 	.text
 	.section	.rodata
-_L0:
+_str:
 	.ascii	"%d\000"
 	.align	2
 	.global	fibo
-fibo:
+fibo:	@ function int fibo(int)
 	push	{r4, fp, lr}
 	add	fp, sp, #8
 	sub	sp, sp, #12
 	str	r0, [fp, #-16]
-	ldr	r3, [fp, #-16]		@一定要将其单独存在r3中,不可直接比较,否则会陷入死循环
+	ldr	r3, [fp, #-16]		@ 一定要将其单独存在r3中,不可直接比较,否则会陷入死循环
 	cmp	r3, #1
 	beq	.L1
 	ldr	r3, [fp, #-16]
@@ -23,12 +22,12 @@ fibo:
 	ldr	r3, [fp, #-16]
 	sub	r3, r3, #1
 	mov	r0, r3
-	bl	fibo
+	bl	fibo		@ 第一次递归，为了防止寄存器调用冲突，使用已经提前保存了数值的寄存器r
 	mov	r4, r0
 	ldr	r3, [fp, #-16]
 	sub	r3, r3, #2
 	mov	r0, r3
-	bl	fibo
+	bl	fibo		@ 第二次递归，同上
 	mov	r3, r0
 	add	r3, r4, r3
 .L3:
@@ -42,15 +41,15 @@ main:
 	push	{fp, lr}
 	add	fp, sp, #4
 	sub	sp, sp, #8
-	mov	r0, #5
+	mov	r0, #5		@ fibo函数，使用参数常数5
 	bl	fibo
 	mov r1, r0
-	ldr r0, _bridge
+	ldr r0, _bridge			@ 将字符串常量地址作为第一个参数，fibo函数返回值保存在r1中，作为第二个参数
 	bl	printf
 	mov	r0, #0
 	sub	sp, fp, #4
 	pop	{fp, pc}
 
 _bridge:
-	.word	_L0
-	.section	.note.GNU-stack,"",%progbits
+	.word	_str
+	.section	.note.GNU-stack,"",%progbits		@ 保护代码，禁止生成可执行堆栈
--- compile_test2.s
+++ compile_test2.s
@@ -1,12 +1,12 @@
-	.arch armv7-a
 	.text
-.section	.rodata
+	.section	.rodata
 _str:
-	.ascii	"%d\012\000"
+	.ascii	"%d\012\000"	@ "%d\n\0"
 
-factorial:
-	str	fp, [sp, #-4]!
-	add	fp, sp, #0
+factorial:	@ int factorial(int k) --> k!
+	str	fp, [sp, #-4]
+	sub sp, sp, #4
+	mov	fp, sp
 	sub	sp, sp, #20
 	str	r0, [fp, #-16]
 	mov	r3, #1
@@ -32,17 +32,17 @@ factorial:
 	ldr	fp, [sp], #4
 	bx	lr
 	
-C:
-	push	{r4, r5, fp, lr}
+C: @ int C(int m,int n) --> C^m_n
+	push {fp, lr}
 	add	fp, sp, #12
 	sub	sp, sp, #8
 	str	r0, [fp, #-16]
 	str	r1, [fp, #-20]
 	ldr	r0, [fp, #-16]
-	bl	factorial
+	bl	factorial		@ 计算m！
 	mov	r5, r0
 	ldr	r0, [fp, #-20]
-	bl	factorial
+	bl	factorial		@ 计算n！
 	mov	r4, r0
 	ldr	r2, [fp, #-16]
 	ldr	r3, [fp, #-20]
@@ -53,11 +53,9 @@ C:
 	mul	r3, r3, r4
 	mov	r1, r3
 	mov	r0, r5
-	bl	__aeabi_idiv
-	mov	r3, r0
-	mov	r0, r3
+	bl	__aeabi_idiv	@ 调用已有函数，计算m!/(m-n)!/n!
 	sub	sp, fp, #12
-	pop	{r4, r5, fp, pc}
+	pop	{fp, pc}
 
 	.global	main
 	.type	main, %function
@@ -74,4 +72,4 @@ main:
 	pop	{fp, pc}
 _bridge:
 	.word	_str
-	.section	.note.GNU-stack,"",%progbits
+	.section	.note.GNU-stack,"",%progbits		@ 保护代码，禁止生成可执行堆栈
